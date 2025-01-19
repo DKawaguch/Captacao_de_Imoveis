56ae0788bac964b00d7ec020fab69a505b452fa5
DKawaguch <dkawaguchi@hotmail.com>
2025-01-19 16:11:52 -0300

    parcialmente funcionando, falta adaptações

---
+++ src/Venda.py
@@ -0,0 +1,194 @@
+from PyQt5.QtWidgets import (
+    QWidget, QLabel, QLineEdit, QVBoxLayout, 
+    QComboBox, QPlainTextEdit, QSpinBox, QCheckBox, QGridLayout
+)
+
+class Venda(QWidget):
+    def __init__(self):
+        super().__init__()
+        #self.setWindowTitle("Venda")
+        layout = QVBoxLayout(self)
+
+        # Campos Exclusivos de Venda
+
+        self.quitado_label = QLabel("Quitado:")
+        self.quitado_combo = QComboBox()
+        self.quitado_combo.addItems(["", "Sim", "Não"])
+        layout.addWidget(self.quitado_label)
+        layout.addWidget(self.quitado_combo)
+
+        self.financiado_label = QLabel("Financiado em Quantas Vezes:")
+        self.financiado_input = QSpinBox()
+        self.financiado_input.setRange(0, 60)
+        layout.addWidget(self.financiado_label)
+        layout.addWidget(self.financiado_input)
+
+        self.parcela_label = QLabel("Parcelas de:")
+        self.parcela_input = QSpinBox()
+        self.parcela_input.setRange(0, 10000000)
+        layout.addWidget(self.parcela_label)
+        layout.addWidget(self.parcela_input)
+
+        # Endereço e localização
+        self.endereco_label = QLabel("Endereço:")
+        self.endereco_input = QLineEdit()
+        layout.addWidget(self.endereco_label)
+        layout.addWidget(self.endereco_input)
+
+        self.bairro_label = QLabel("Bairro:")
+        self.bairro_input = QLineEdit()
+        layout.addWidget(self.bairro_label)
+        layout.addWidget(self.bairro_input)
+
+        self.cidade_label = QLabel("Cidade:")
+        self.cidade_input = QLineEdit()
+        layout.addWidget(self.cidade_label)
+        layout.addWidget(self.cidade_input)
+
+        self.uf_label = QLabel("UF:")
+        self.uf_combo = QComboBox()
+        self.uf_combo.addItems(["", "AC", "AL", "AP", "AM", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RS", "RO", "RR", "SC", "SP", "SE", "TO"])
+        layout.addWidget(self.uf_label)
+        layout.addWidget(self.uf_combo)
+
+        # Áreas
+        self.area_total_label = QLabel("Área Total (m²):")
+        self.area_total_input = QSpinBox()
+        self.area_total_input.setRange(0, 100000)
+        layout.addWidget(self.area_total_label)
+        layout.addWidget(self.area_total_input)
+
+        self.area_util_label = QLabel("Área Útil (m²):")
+        self.area_util_input = QSpinBox()
+        self.area_util_input.setRange(0, 100000)
+        layout.addWidget(self.area_util_label)
+        layout.addWidget(self.area_util_input)
+
+        self.area_construida_label = QLabel("Área Construída (m²):")
+        self.area_construida_input = QSpinBox()
+        self.area_construida_input.setRange(0, 100000)
+        layout.addWidget(self.area_construida_label)
+        layout.addWidget(self.area_construida_input)
+
+        # Valor e Proprietário
+        self.valor_label = QLabel("Valor (R$):")
+        self.valor_input = QSpinBox()
+        self.valor_input.setRange(0, 100000000)
+        layout.addWidget(self.valor_label)
+        layout.addWidget(self.valor_input)
+
+        self.iptu_label = QLabel("IPTU (R$):")
+        self.iptu_input = QSpinBox()
+        self.iptu_input.setRange(0, 1000000)
+        layout.addWidget(self.iptu_label)
+        layout.addWidget(self.iptu_input)
+
+        self.condominio_label = QLabel("Condomínio (R$):")
+        self.condominio_input = QSpinBox()
+        self.condominio_input.setRange(0, 1000000)
+        layout.addWidget(self.condominio_label)
+        layout.addWidget(self.condominio_input)
+
+        # Tipo de Imóvel
+        self.tipo_imovel_label = QLabel("Tipo de Imóvel:")
+        self.tipo_imovel_combo = QComboBox()
+        self.tipo_imovel_combo.addItems(["", "Casa Térrea", "Sobrado", "Apartamento", "Terreno", "Sítio",
+        "Chácara", "Galpão", "Assobradado", "Fazenda", "Indústria"])
+        layout.addWidget(self.tipo_imovel_label)
+        layout.addWidget(self.tipo_imovel_combo)
+
+        # Características
+        self.caracteristicas_label = QLabel("Características:")
+        self.caracteristicas_layout = QGridLayout()
+        self.caracteristicas_checkboxes = {
+            "Dormitórios": QCheckBox("Dormitórios"),
+            "Suítes": QCheckBox("Suítes"),
+            "Closet": QCheckBox("Closet"),
+            "Sacada": QCheckBox("Sacada"),
+            "Cozihna": QCheckBox("Cozinha"),
+            "Despensa": QCheckBox("Despensa"),
+            "Armários": QCheckBox("Armários"),
+            "Copa": QCheckBox("Copa"),
+            "Sala": QCheckBox("Sala"),
+            "Lareira": QCheckBox("Lareira"),
+            "Sala de Jantar": QCheckBox("Sala de Jantar"),
+            "Lavabo": QCheckBox("Lavabo"),
+            "Lavanderia": QCheckBox("Lavanderia"),
+            "Dependência de Empregada": QCheckBox("Dependência de Empregada"),
+            "W.C. de Empregada": QCheckBox("W.C. de Empregada"),
+            "Banheiros": QCheckBox("Banheiros"),
+            "Hidromassagem": QCheckBox("Hidromassagem"),
+            "Aquecedor": QCheckBox("Aquecedor"),
+            "Gás Encanado": QCheckBox("Gás Encanado"),
+            "Área de Serviço": QCheckBox("Área de Serviço"),
+            "Quintal": QCheckBox("Quintal"),
+            "Playground": QCheckBox("Playground"),
+            "Quadra Poliesportiva": QCheckBox("Quadra Poliesportiva"),
+            "Piscina": QCheckBox("Piscina"),
+            "Salão de Festas": QCheckBox("Salão de Festas"),	
+            "Salão de Jogos": QCheckBox("Salão de Jogos"),
+            "Sala de Ginástica": QCheckBox("Sala de Ginástica"),
+            "Sauna": QCheckBox("Sauna"),
+            "Churrasqueira": QCheckBox("Churrasqueira"),
+            "Garagem": QCheckBox("Garagem"),
+            "Edicula": QCheckBox("Edicula")
+        }
+
+        row = 0
+        for i, (key, checkbox) in enumerate(self.caracteristicas_checkboxes.items()):
+            self.caracteristicas_layout.addWidget(checkbox, row, i % 7)
+            if i % 7 == 1:
+                row += 1
+
+        layout.addWidget(self.caracteristicas_label)
+        layout.addLayout(self.caracteristicas_layout)
+
+        self.nome_proprietario_label = QLabel("Nome do Proprietário:")
+        self.nome_proprietario_input = QLineEdit()
+        layout.addWidget(self.nome_proprietario_label)
+        layout.addWidget(self.nome_proprietario_input)
+
+        self.telefone_proprietario_label = QLabel("Telefone do Proprietário:")
+        self.telefone_proprietario_input = QLineEdit()
+        layout.addWidget(self.telefone_proprietario_label)
+        layout.addWidget(self.telefone_proprietario_input)
+
+        self.email_proprietario_label = QLabel("E-mail do Proprietário:")
+        self.email_proprietario_input = QLineEdit()
+        layout.addWidget(self.email_proprietario_label)
+        layout.addWidget(self.email_proprietario_input)
+
+        # Observações
+        self.observacoes_label = QLabel("Observações:")
+        self.observacoes_input = QPlainTextEdit()
+        layout.addWidget(self.observacoes_label)
+        layout.addWidget(self.observacoes_input)
+
+    def collect_data(self):
+        return{
+            
+            "tipo_operacao": "Venda",
+            "fiador": None,
+            "seguro_fianca": None,
+            "adiantamento_alugueis": 0,
+            "quitado": self.quitado_combo.currentText(),
+            "financiamento_qtd_parcelas": self.financiado_input.value(),
+            "financiamento_valor_parcela": self.parcela_input.value(),
+            "endereco": self.endereco_input.text(),
+            "bairro": self.bairro_input.text(),
+            "cidade": self.cidade_input.text(),
+            "uf": self.uf_combo.currentText(),
+            "area_total": self.area_total_input.value(),
+            "area_util": self.area_util_input.value(),
+            "area_construida": self.area_construida_input.value(),
+            "valor": self.valor_input.value(),
+            "iptu": self.iptu_input.value(),
+            "condominio": self.condominio_input.value(),
+            "tipo_imovel": self.tipo_imovel_combo.currentText(),
+            "caracteristicas": ", ".join([key for key, checkbox in self.caracteristicas_checkboxes.items() if checkbox.isChecked()]),
+            "nome_proprietario": self.nome_proprietario_input.text(),
+            "telefone_proprietario": self.telefone_proprietario_input.text(),
+            "email": self.email_proprietario_input.text(),
+            "observacoes": self.observacoes_input.toPlainText()
+
+        }
--- src/imovel_app.py
+++ src/imovel_app.py
@@ -5,9 +5,18 @@ import mysql.connector
 from mysql.connector import Error
 from dotenv import load_dotenv
 from PyQt5.QtWidgets import (
-    QApplication, QWidget, QLabel, QLineEdit, QVBoxLayout, QPushButton, 
-    QComboBox, QPlainTextEdit, QSpinBox, QMessageBox, QCheckBox, QGridLayout
+    QApplication, 
+    QWidget, 
+    QLabel, 
+    QVBoxLayout, 
+    QPushButton, 
+    QComboBox, 
+    QMessageBox, 
+    QMainWindow
 )
+
+from Venda import Venda
+from Locacao import Locacao
 
 load_dotenv()
 
@@ -47,6 +56,12 @@ def initialize_database():
             CREATE TABLE IF NOT EXISTS imoveis (
                 id INT AUTO_INCREMENT PRIMARY KEY,
                 tipo_operacao VARCHAR(50) NOT NULL,
+                fiador VARCHAR(20) DEFAULT 'Não se aplica',
+                seguro_fianca VARCHAR(20) DEFAULT 'Não se aplica',
+                adiantamento_alugueis INT DEFAULT 0,
+                quitado VARCHAR(20 ) DEFAULT 'Não se aplica',
+                financiamento_qtd_parcelas INT DEFAULT 0,
+                financiamento_valor_parcela FLOAT DEFAULT 0.0,
                 endereco VARCHAR(255) NOT NULL,
                 bairro VARCHAR(100) NOT NULL,
                 cidade VARCHAR(100) NOT NULL,
@@ -73,259 +88,103 @@ def initialize_database():
         if conn.is_connected():
             conn.close()
 
-class ImovelApp(QWidget):
+class ImovelApp(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Cadastro de Imóveis")
-        self.initUI()
+        self.setWindowTitle("Cadastro do Imóvel")
+        #self.initUI()
 
-    def initUI(self):
-        layout = QVBoxLayout()
+        # Layout principal
+        self.central_widget = QWidget()
+        self.setCentralWidget(self.central_widget)
+        self.layout = QVBoxLayout(self.central_widget)
 
         # Tipo de Operação
         self.tipo_operacao_label = QLabel("Tipo de Operação:")
         self.tipo_operacao_combo = QComboBox()
         self.tipo_operacao_combo.addItems(["", "Locação", "Venda"])
-        layout.addWidget(self.tipo_operacao_label)
-        layout.addWidget(self.tipo_operacao_combo)
+        self.layout.addWidget(self.tipo_operacao_label)
+        self.layout.addWidget(self.tipo_operacao_combo)
 
-        # Separando diferenças entre locação e venda
-        if self.tipo_operacao_combo.currentText() == "Locação":
-            self.com_fiador_label = QLabel("Com Fiador:")
-            self.com_fiador_combo = QComboBox()
-            self.com_fiador_combo.addItems(["", "Sim", "Não"])
-            layout.addWidget(self.com_fiador_label)
-            layout.addWidget(self.com_fiador_combo)
-
-            self.seguro_fianca_label = QLabel("Seguro Fiança:")
-            self.seguro_fianca_combo = QComboBox()
-            self.seguro_fianca_combo.addItems(["", "Sim", "Não"])
-            layout.addWidget(self.seguro_fianca_label)
-            layout.addWidget(self.seguro_fianca_combo)
-
-            self.adiantamento_alugueis_label = QLabel("Adiantamento de Quantos Aluguéis:")
-            self.adiantamento_alugueis_input = QSpinBox()
-            self.adiantamento_alugueis_input.setRange(0, 12)
-            layout.addWidget(self.adiantamento_alugueis_label)
-            layout.addWidget(self.adiantamento_alugueis_input)
-
-        elif self.tipo_operacao_combo.currentText() == "Venda":
-            self.quitado_label = QLabel("Quitado:")
-            self.quitado_combo = QComboBox()
-            self.quitado_combo.addItems(["", "Sim", "Não"])
-            layout.addWidget(self.quitado_label)
-            layout.addWidget(self.quitado_combo)
-
-            self.financiado_label = QLabel("Financiado em Quantas Vezes:")
-            self.financiado_input = QSpinBox()
-            self.financiado_input.setRange(0, 60)
-            layout.addWidget(self.financiado_label)
-            layout.addWidget(self.financiado_input)
-
-            self.parcela_label = QLabel("Parcelas de:")
-            self.parcela_input = QSpinBox()
-            self.parcela_input.setRange(0, 1000000)
-            layout.addWidget(self.parcela_label)
-            layout.addWidget(self.parcela_input)
-
-        # Endereço e localização
-        self.endereco_label = QLabel("Endereço:")
-        self.endereco_input = QLineEdit()
-        layout.addWidget(self.endereco_label)
-        layout.addWidget(self.endereco_input)
-
-        self.bairro_label = QLabel("Bairro:")
-        self.bairro_input = QLineEdit()
-        layout.addWidget(self.bairro_label)
-        layout.addWidget(self.bairro_input)
-
-        self.cidade_label = QLabel("Cidade:")
-        self.cidade_input = QLineEdit()
-        layout.addWidget(self.cidade_label)
-        layout.addWidget(self.cidade_input)
-
-        self.uf_label = QLabel("UF:")
-        self.uf_combo = QComboBox()
-        self.uf_combo.addItems(["", "AC", "AL", "AP", "AM", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RS", "RO", "RR", "SC", "SP", "SE", "TO"])
-        layout.addWidget(self.uf_label)
-        layout.addWidget(self.uf_combo)
-
-        # Áreas
-        self.area_total_label = QLabel("Área Total (m²):")
-        self.area_total_input = QSpinBox()
-        self.area_total_input.setRange(0, 100000)
-        layout.addWidget(self.area_total_label)
-        layout.addWidget(self.area_total_input)
-
-        self.area_util_label = QLabel("Área Útil (m²):")
-        self.area_util_input = QSpinBox()
-        self.area_util_input.setRange(0, 100000)
-        layout.addWidget(self.area_util_label)
-        layout.addWidget(self.area_util_input)
-
-        self.area_construida_label = QLabel("Área Construída (m²):")
-        self.area_construida_input = QSpinBox()
-        self.area_construida_input.setRange(0, 100000)
-        layout.addWidget(self.area_construida_label)
-        layout.addWidget(self.area_construida_input)
-
-        # Valor e Proprietário
-        self.valor_label = QLabel("Valor (R$):")
-        self.valor_input = QSpinBox()
-        self.valor_input.setRange(0, 100000000)
-        layout.addWidget(self.valor_label)
-        layout.addWidget(self.valor_input)
-
-        self.iptu_label = QLabel("IPTU (R$):")
-        self.iptu_input = QSpinBox()
-        self.iptu_input.setRange(0, 1000000)
-        layout.addWidget(self.iptu_label)
-        layout.addWidget(self.iptu_input)
-
-        self.condominio_label = QLabel("Condomínio (R$):")
-        self.condominio_input = QSpinBox()
-        self.condominio_input.setRange(0, 1000000)
-        layout.addWidget(self.condominio_label)
-        layout.addWidget(self.condominio_input)
-
-        # Tipo de Imóvel
-        self.tipo_imovel_label = QLabel("Tipo de Imóvel:")
-        self.tipo_imovel_combo = QComboBox()
-        self.tipo_imovel_combo.addItems(["", "Casa Térrea", "Sobrado", "Apartamento", "Terreno", "Sítio",
-        "Chácara", "Galpão", "Assobradado", "Fazenda", "Indústria"])
-        layout.addWidget(self.tipo_imovel_label)
-        layout.addWidget(self.tipo_imovel_combo)
-
-        # Características
-        self.caracteristicas_label = QLabel("Características:")
-        self.caracteristicas_layout = QGridLayout()
-        self.caracteristicas_checkboxes = {
-            "Dormitórios": QCheckBox("Dormitórios"),
-            "Suítes": QCheckBox("Suítes"),
-            "Closet": QCheckBox("Closet"),
-            "Sacada": QCheckBox("Sacada"),
-            "Cozihna": QCheckBox("Cozinha"),
-            "Despensa": QCheckBox("Despensa"),
-            "Armários": QCheckBox("Armários"),
-            "Copa": QCheckBox("Copa"),
-            "Sala de Almoço": QCheckBox("Sala de Almoço"),
-            "Sala": QCheckBox("Sala"),
-            "Lareira": QCheckBox("Lareira"),
-            "Sala de Jantar": QCheckBox("Sala de Jantar"),
-            "Lavabo": QCheckBox("Lavabo"),
-            "Lavanderia": QCheckBox("Lavanderia"),
-            "Dependência de Empregada": QCheckBox("Dependência de Empregada"),
-            "W.C. de Empregada": QCheckBox("W.C. de Empregada"),
-            "Banheiros": QCheckBox("Banheiros"),
-            "Hidromassagem": QCheckBox("Hidromassagem"),
-            "Aquecedor": QCheckBox("Aquecedor"),
-            "Gás Encanado": QCheckBox("Gás Encanado"),
-            "Área de Serviço": QCheckBox("Área de Serviço"),
-            "Quintal": QCheckBox("Quintal"),
-            "Playground": QCheckBox("Playground"),
-            "Quadra Poliesportiva": QCheckBox("Quadra Poliesportiva"),
-            "Piscina": QCheckBox("Piscina"),
-            "Salão de Festas": QCheckBox("Salão de Festas"),	
-            "Salão de Jogos": QCheckBox("Salão de Jogos"),
-            "Sala de Ginástica": QCheckBox("Sala de Ginástica"),
-            "Sauna": QCheckBox("Sauna"),
-            "Churrasqueira": QCheckBox("Churrasqueira"),
-            "Garagem": QCheckBox("Garagem"),
-            "Edicula": QCheckBox("Edicula"),
-            "Placa Solar": QCheckBox("Placa Solar"),
-            "Chaves": QCheckBox("Chaves"),
-        }
-
-        row = 0
-        for i, (key, checkbox) in enumerate(self.caracteristicas_checkboxes.items()):
-            self.caracteristicas_layout.addWidget(checkbox, row, i % 4)
-            if i % 4 == 1:
-                row += 1
-
-        layout.addWidget(self.caracteristicas_label)
-        layout.addLayout(self.caracteristicas_layout)
-
-        self.nome_proprietario_label = QLabel("Nome do Proprietário:")
-        self.nome_proprietario_input = QLineEdit()
-        layout.addWidget(self.nome_proprietario_label)
-        layout.addWidget(self.nome_proprietario_input)
-
-        self.telefone_proprietario_label = QLabel("Telefone do Proprietário:")
-        self.telefone_proprietario_input = QLineEdit()
-        layout.addWidget(self.telefone_proprietario_label)
-        layout.addWidget(self.telefone_proprietario_input)
-
-        self.email_proprietario_label = QLabel("E-mail do Proprietário:")
-        self.email_proprietario_input = QLineEdit()
-        layout.addWidget(self.email_proprietario_label)
-        layout.addWidget(self.email_proprietario_input)
-
-        # Observações
-        self.observacoes_label = QLabel("Observações:")
-        self.observacoes_input = QPlainTextEdit()
-        layout.addWidget(self.observacoes_label)
-        layout.addWidget(self.observacoes_input)
+        # Placeholder para o formulário dinâmico
+        self.form_placeholder = QWidget()
+        self.form_layout = QVBoxLayout(self.form_placeholder)
+        self.layout.addWidget(self.form_placeholder)
 
         # Botões
         self.salvar_button = QPushButton("Salvar")
         self.salvar_button.clicked.connect(self.save_data)
-        layout.addWidget(self.salvar_button)
+        self.layout.addWidget(self.salvar_button)
 
-        self.setLayout(layout)
+        #self.setLayout(layout)
+
+        # Conectar a mudança no ComboBox
+        self.tipo_operacao_combo.currentIndexChanged.connect(self.show_form)
+
+        # Variável para armazenar o formulário atual
+        self.current_form = None
+
+    def show_form(self):
+        # Remover o formulário atual, se houver
+        if self.current_form:
+            self.current_form.setParent(None)
+            self.current_form = None
+
+        # Determinar qual formulário exibir
+        operation = self.tipo_operacao_combo.currentText()
+
+        if operation == "Venda":
+            self.current_form = Venda()
+        elif operation == "Locação":
+            self.current_form = Locacao()
+
+        # Adicionar o novo formulário ao layout
+        if self.current_form:
+            self.form_layout.addWidget(self.current_form)
 
     def save_data(self):
-        data = {
-            "tipo_operacao": self.tipo_operacao_combo.currentText(),
-            "endereco": self.endereco_input.text(),
-            "bairro": self.bairro_input.text(),
-            "cidade": self.cidade_input.text(),
-            "uf": self.uf_combo.currentText(),
-            "area_total": self.area_total_input.value(),
-            "area_util": self.area_util_input.value(),
-            "area_construida": self.area_construida_input.value(),
-            "valor": self.valor_input.value(),
-            "iptu": self.iptu_input.value(),
-            "condominio": self.condominio_input.value(),
-            "tipo_imovel": self.tipo_imovel_combo.currentText(),
-            "caracteristicas": ", ".join([key for key, checkbox in self.caracteristicas_checkboxes.items() if checkbox.isChecked()]),
-            "nome_proprietario": self.nome_proprietario_input.text(),
-            "telefone_proprietario": self.telefone_proprietario_input.text(),
-            "email": self.email_proprietario_input.text(),
-            "observacoes": self.observacoes_input.toPlainText()
-        }
-
-        try:
-            conn = get_connection()
-            cursor = conn.cursor()
-            cursor.execute('''
-                INSERT INTO imoveis (
-            tipo_operacao, 
-            endereco, 
-            bairro, 
-            cidade, 
-            uf, 
-            area_total, 
-            area_util, 
-            area_construida,
-            valor, 
-            iptu, 
-            condominio, 
-            tipo_imovel, 
-            caracteristicas, 
-            nome_proprietario,
-            telefone_proprietario, 
-            email_proprietario, 
-            observacoes
-        ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-            ''', tuple(data.values()))
-            conn.commit()
-            QMessageBox.information(self, "Sucesso", "Dados salvos com sucesso!")
-        except Error as e:
-            QMessageBox.critical(self, "Erro", f"Erro ao salvar dados: {e}")
-            logging.error(f"Erro ao salvar dados: {e}")
-        finally:
-            if conn.is_connected():
-                conn.close()
+        if self.current_form:
+            try:
+                data = self.current_form.collect_data()  # Método nos formulários que retorna os dados
+                conn = get_connection()
+                cursor = conn.cursor()
+                cursor.execute('''
+                    INSERT INTO imoveis (
+                        tipo_operacao, 
+                        fiador,
+                        seguro_fianca,
+                        adiantamento_alugueis,
+                        quitado,
+                        financiamento_qtd_parcelas,
+                        financiamento_valor_parcela,
+                        endereco, 
+                        bairro, 
+                        cidade, 
+                        uf, 
+                        area_total, 
+                        area_util, 
+                        area_construida,
+                        valor, 
+                        iptu, 
+                        condominio, 
+                        tipo_imovel, 
+                        caracteristicas, 
+                        nome_proprietario,
+                        telefone_proprietario, 
+                        email_proprietario, 
+                        observacoes
+                    ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+                ''', tuple(data.values()))
+                conn.commit()
+                QMessageBox.information(self, "Sucesso", "Dados salvos com sucesso!")
+            except Error as e:
+                QMessageBox.critical(self, "Erro", f"Erro ao salvar dados: {e}")
+                logging.error(f"Erro ao salvar dados: {e}")
+            finally:
+                if conn.is_connected():
+                    conn.close()
+        else:
+            QMessageBox.warning(self, "Aviso", "Nenhum formulário foi selecionado.")
 
 if __name__ == "__main__":
     initialize_database()
